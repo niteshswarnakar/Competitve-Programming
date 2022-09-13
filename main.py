498ca407113419c36ac3b0078984ddda7cae2be7
niteshswarnakar <nitesh.swarnakar1@gmail.com>
2022-09-13 09:06:59 +0545

    bracket symmetry checking code added

--- main.py
+++ main.py
@@ -4,30 +4,47 @@ from doctest import FAIL_FAST
 #Q1. Find which rooms are opened at the end when each customer enter into the building
 #and they change door state for their multiplicative room number upto 100
 
-rooms = [False for i in range(100)]
+# rooms = [False for i in range(100)]
 
-def find_room_state(rooms):
-    for i in range(100):
-        for j in range(i,100,i+1):
-            if rooms[j] == False:
-                rooms[j] = True
+# def find_room_state(rooms):
+#     for i in range(100):
+#         for j in range(i,100,i+1):
+#             if rooms[j] == False:
+#                 rooms[j] = True
+#             else:
+#                 rooms[j] = False
+#         # print(rooms[i])
+#     return rooms
+
+
+# final_rooms = find_room_state(rooms)
+
+# i = 1
+
+# for room in rooms:
+#     if room == True:
+#         print(i,"(Open) , ",end = "")
+#     else:
+#         print(i,",", end = "")
+#     i+=1
+
+def check_brackets():
+    characters = input("Enter the bracket string : ")
+    result = []
+    for character in characters:
+        if character == "(":
+            result.append(character)
+        elif character == ")":
+            if len(result) == 0:
+                return "not match"
             else:
-                rooms[j] = False
-        # print(rooms[i])
-    return rooms
-
-
-final_rooms = find_room_state(rooms)
-
-i = 1
-
-for room in rooms:
-    if room == True:
-        print(i,"(Open) , ",end = "")
+                result.pop()
+    if len(result) == 0:
+        return "matched"
     else:
-        print(i,",", end = "")
-    i+=1
+        return "not matched"
 
+print(check_brackets())
 
 
 
