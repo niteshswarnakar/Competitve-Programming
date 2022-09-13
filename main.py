adbcd60428b5da8119143b0a5c2a0fc7e4170ba1
niteshswarnakar <nitesh.swarnakar1@gmail.com>
2022-09-13 09:11:42 +0545

    third commit

--- main.py
+++ main.py
@@ -4,29 +4,29 @@ from doctest import FAIL_FAST
 #Q1. Find which rooms are opened at the end when each customer enter into the building
 #and they change door state for their multiplicative room number upto 100
 
-# rooms = [False for i in range(100)]
 
-# def find_room_state(rooms):
-#     for i in range(100):
-#         for j in range(i,100,i+1):
-#             if rooms[j] == False:
-#                 rooms[j] = True
-#             else:
-#                 rooms[j] = False
-#         # print(rooms[i])
-#     return rooms
+def find_room_state():
+    rooms = [False for i in range(100)]
+    for i in range(100):
+        for j in range(i,100,i+1):
+            if rooms[j] == False:
+                rooms[j] = True
+            else:
+                rooms[j] = False
+    i = 0
+    for room in rooms:
+        if room == True:
+            print(i,"(Open) , ",end = "")
+        else:
+            print(i,",", end = "")
+        i+=1
+    return rooms
 
 
-# final_rooms = find_room_state(rooms)
+# final_rooms = find_room_state()
 
-# i = 1
 
-# for room in rooms:
-#     if room == True:
-#         print(i,"(Open) , ",end = "")
-#     else:
-#         print(i,",", end = "")
-#     i+=1
+#bracket symmetry checking 
 
 def check_brackets():
     characters = input("Enter the bracket string : ")
@@ -44,7 +44,7 @@ def check_brackets():
     else:
         return "not matched"
 
-print(check_brackets())
+# print(check_brackets())
 
 
 
