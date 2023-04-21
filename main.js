7cd5d34815ca6a1149da8b568e059f953c4e0d4b
niteshswarnakar <nitesh.swarnakar1@gmail.com>
2023-04-21 13:18:52 +0545

    change main.js

--- main.js
+++ main.js
@@ -1,18 +1,44 @@
-let text = "iamniteshswarnakar";
-let myarr = text.split("");
+// Exclude the repeatative elements from the array
 
-function CleanArray(arr) {
-  let tempArray = [];
-  let resultArray = [];
-  arr.forEach((element) => {
-    if (resultArray.includes(element)) {
-      tempArray.push(element);
-      resultArray = resultArray.filter((item) => item !== element);
-    } else if (!tempArray.includes(element)) {
-      resultArray.push(element);
+// let text = "iamniteshswarnakar";
+// let myarr = text.split("");
+
+// function CleanArray(arr) {
+//   let tempArray = [];
+//   let resultArray = [];
+//   arr.forEach((element) => {
+//     if (resultArray.includes(element)) {
+//       tempArray.push(element);
+//       resultArray = resultArray.filter((item) => item !== element);
+//     } else if (!tempArray.includes(element)) {
+//       resultArray.push(element);
+//     }
+//   });
+//   return resultArray;
+// }
+
+// console.log(CleanArray(myarr));
+
+// selection sort
+
+let myarr = [3, 6, 1, 2, 9, 67, 7];
+
+const selection_sort = (arr) => {
+  let i = 0;
+  let min_index;
+  let j;
+  for (i = 0; i < arr.length - 1; i++) {
+    min_index = i;
+    for (j = i; j < arr.length; j++) {
+      if (arr[j] < arr[min_index]) {
+        min_index = j;
+      }
     }
-  });
-  return resultArray;
-}
+    let temp = arr[i];
+    arr[i] = arr[min_index];
+    arr[min_index] = temp;
+  }
+  return arr;
+};
 
-console.log(CleanArray(myarr));
+console.log(selection_sort(myarr));
